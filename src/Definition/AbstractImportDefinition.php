c4ff1a7908625b0db82fe5325f8d3ce3aa32330e
Maurizio Monticelli <rugbymauri@gmail.com>
2022-01-25 11:00:36 +0100

    feat: inject entity manager

--- src/Definition/AbstractImportDefinition.php
+++ src/Definition/AbstractImportDefinition.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace whatwedo\ImportBundle\Definition;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 abstract class AbstractImportDefinition implements ImportDefinitionInterface
 {
+    protected EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     abstract public function getEntityClass(): string;
 
     public function createEntity(array $data): object
